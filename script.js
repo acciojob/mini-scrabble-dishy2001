335b6cfbb91bffaa049a8af8352ecc5fce381bd7
Deekshya Karki <99348458+dishy2001@users.noreply.github.com>
2022-12-26 20:47:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+let stringInput=document.getElementById("stringInput");
+let length=document.getElementById("length");
+stringInput.addEventListener('keyup',(e)=>{
+  if(e.key !=="Escape"){
+  let str=stringInput.value;
+  length.innerHTML=str.length;
+  console.log(str.length);
+  }
+});
