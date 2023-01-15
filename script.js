1fd07ccb4527cb554a04509f26b4c0f8b8a7319a
Deekshya Karki <99348458+dishy2001@users.noreply.github.com>
2023-01-15 21:27:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 //your code here
-let stringInput=document.getElementById("evaluatedText");
-let length=document.getElementById("length");
-stringInput.addEventListener('keypress',(e)=>{
-  // if(e.key !=="Escape"){
-  let str=stringInput.value;
-	if(str == null){
+	let stringInput=document.getElementById("evaluatedText");
+	
+	let leng=0;
+stringInput.addEventListener('keydown',(e)=>{
+	let len=document.getElementById("length").textContent;
+	console.log(len);
+
+	if(stringInput.value == null){
+		document.getElementById("length").textContent=0;
 		return;
 	}
-  length.innerHTML=str.length+1;
+	if(e.key =="Backspace"){
+		if(len == 0)
+			return;
+		document.getElementById("length").textContent =   parseInt(len) - 1;
+		return;
+	}
+	  document.getElementById("length").textContent = stringInput.value.length+1;
   // }
 });
