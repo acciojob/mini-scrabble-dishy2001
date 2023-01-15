79f24300be76bb03afaa6c75059992b1c5d971a3
Deekshya Karki <99348458+dishy2001@users.noreply.github.com>
2023-01-15 20:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //your code here
 let stringInput=document.getElementById("evaluatedText");
 let length=document.getElementById("length");
-stringInput.addEventListener('keyup',(e)=>{
-  if(e.key !=="Escape"){
+stringInput.addEventListener('keypress',(e)=>{
+  // if(e.key !=="Escape"){
   let str=stringInput.value;
-  length.innerHTML=str.length;
-  }
+	if(str == null){
+		return;
+	}
+  length.innerHTML=str.length+1;
+  // }
 });
